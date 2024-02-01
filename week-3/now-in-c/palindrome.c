df90ccd63dc2a9052de7a6ef873fe25f8846fb43
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-01 01:28:11 +0000

    wtf even is this error

--- week-3/now-in-c/palindrome.c
+++ week-3/now-in-c/palindrome.c
@@ -12,14 +12,30 @@ int is_exact(char *message);
 
 
 int main(int argc, char **argv) {
+	// debug options
+	char *message = "tacoc.at";
+	argc = 2;
+
 	if (argc > 1) {
-		char *message = argv[1];
-		
+		// char *message = argv[1];
+
+		// type[0]: is this an exact palindrome?
+		// type[1]: is this a palindrome at all?
+		// int type[2];
+		// type[0] = is_exact(message);
+
+		if (is_exact(message) == 1) {
+			printf("This is a palindrome.\n");
+		} else {
+			if (is_palindrome(message) == 1) {
+				printf("This is an inexact palindrome.");
+			} else {
+				printf("Sorry, this is not a palindrome.");
+			}
+		}
 	} else {
 		printf("While an empty string IS technically a palindrome, it would be nice if you gave me a proper string to test.\n");
 	}
-
-
 
 	return 0;
 }
@@ -62,28 +78,54 @@ int array_contains(char *message, int message_length, char item) {
 // strips out any punctuation character
 char *strip_punct(char *message) {
 	int message_length;
-	message_length = strlen(message);
+	message_length = strlen(message) + 10;
 
-	char *message_punctless;
+	// char *iter_message;
+	// iter_message = message;
+
+	char message_array[message_length];
+	snprintf(message_array, "%s\0", (message_length), message);
+
+
+	char message_punctless[message_length];
+	int count_not_punct = 0;
 
 	char punct[11] = {',', '.', ':', ';', '\'', '"', '!', '?', '/', '\\', ' '};
 
 	int i;
-	for (i = 0; i < message_length; i++, message++) {
-		if (array_contains(message, message_length, punct[i]) == 0) {
-			*message_punctless == *message;
-			message_punctless++;
+	for (i = 0; i < message_length; i++) {
+		int contains_this_punct = array_contains(message, message_length, punct[i]);
+
+		if (contains_this_punct == 0) {
+			char char_to_save = message_array[i];
+			message_punctless[count_not_punct] == char_to_save;
+			count_not_punct++;
 		}
 	}
+
+	// add the string terminator to mark the end of string, regardless of array size, then return
+	message_punctless[count_not_punct] = "\0";
+	char *pointer_message_punctless = message_punctless;
+	return pointer_message_punctless;
 }
 
 
 // checks if the message is a palindrome of any kind
 int is_palindrome(char *message) {
-	int message_length;
-	message_length = strlen(message);
+	char *message_punctless = strip_punct(message);
+	char *reversed_message_punctless = reverse_array(message_punctless);
 
-	// TODO: is reversed_message_punctless == message_punctless?
+	int message_punctless_length;
+	message_punctless_length = strlen(message_punctless);
+
+	int i;
+	for (i = 0; i < message_punctless_length; i++, message_punctless++, reversed_message_punctless++) {
+		if (*message != *reversed_message_punctless) {
+			return 0;
+		}
+	}
+
+	return 1;
 }
 
 
@@ -92,9 +134,14 @@ int is_exact(char *message) {
 	int message_length;
 	message_length = strlen(message);
 
-	char *flipped = reverse_array(message);
+	char *reversed_message = reverse_array(message);
 
-	// TODO: is reversed_message == message?
+	int i;
+	for (i = 0; i < message_length; i++, message++, reversed_message++) {
+		if (*message != *reversed_message) {
+			return 0;
+		}
+	}
 
 	return 1;
 }
