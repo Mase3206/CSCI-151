bb6a0a722f196b3856bfd7980e2fe94280093ff1
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-14 17:27:59 +0000

    set initial variables

---
+++ week-5/practice/race.py
@@ -0,0 +1,18 @@
+import stddraw, race
+
+
+refreshRate = 30
+
+# set race rules
+laps = 3
+baseSpeed = 0.0
+variance = 1.0  # percent difference between base speed and random variation speed
+
+# set car properties
+carRadius = 0.05
+car1_color = stddraw.BLUE
+car2_color = stddraw.RED
+car1_startLoc = (0, 0)
+car2_startLoc = (0, 0)
+
+
