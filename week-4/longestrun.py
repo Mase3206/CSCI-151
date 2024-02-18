89662db515a379ff98a23c599541c2f728c10728
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-18 23:45:23 +0000

    begin assignment 5

---
+++ week-4/longestrun.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# longestrun.py
+#
+# Reads a sequence of integers and writes both the integer that appears in a
+# longest consecutive run and the length of the run
+#
+# Noah S. Roberts
+# 02/18/2024
+# Assignment 5
+# for Week 4
+# Book Excercise 1.5.5
+# =============================================================================
+
+import stdio, stdarray
