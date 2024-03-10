ff543b5af7f55ed2d4cfc12bc9a899a030619e54
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-03-10 22:35:59 +0000

    create median(), _testClient()

--- module-6/helpful.py
+++ module-6/helpful.py
@@ -23,8 +23,63 @@ def avg(numbers: list[int|float]) -> float:
 	
 	Returns
 	-------
-		float average
+		float
 	"""
+
 	return sum(numbers) / len(numbers)
 
 
+def median(numbers: list[int|float]) -> int|float:
+	"""
+	Returns the median of the given list
+
+	Argument
+	--------
+		numbers: list of floats or ints
+
+	Returns
+	-------
+		int or float
+	"""
+
+	if len(numbers) % 2 == 0:
+		# if even
+		low = numbers[len(numbers) / 2 - 1]
+		high = numbers[len(numbers) / 2]
+
+		return avg([low, high])
+	
+	else:
+		# if odd
+		return numbers[len(numbers) / 2 - 0.5]
+	
+
+def _test():
+	"""
+	NOT PART OF THE API.
+	--------------------
+
+	Simple test client. 
+	"""
+
+	import stdio, stdarray
+
+
+	odd = stdarray.create1D(5, 0)
+	odd = [0, 1, 2, 3, 4]
+
+	even = stdarray.create1D(6, 0)
+	even = [0, 1, 2, 3, 4, 5]
+
+
+	stdio.writeln('\n--- Helpful.py Test Client ---')
+	stdio.writeln('NOTE: This function is not part of the API and is only here for testing purposes.\n')
+	stdio.writeln('All values printed first as an integer and later as a float fix 4.')
+
+	stdio.writef('avg(odd):\n\tint: %d\n\tfloat: %.4f\n', avg(odd), avg(odd))
+	stdio.writef('avg(even):\n\tint: %d\n\tfloat: %.4f\n', avg(even), avg(even))
+
+	stdio.writef('median(odd):\n\tint: %d\n\tfloat: %.4f\n', median(odd), median(odd))
+	stdio.writef('median(even):\n\tint: %d\n\tfloat: %.4f\n', median(even), median(even))
+
+	stdio.writeln('--- End Test Client ---\n')
