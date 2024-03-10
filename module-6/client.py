461322c34727317fb296db8270ca5a1cff641e59
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-03-10 22:43:26 +0000

    add header

---
+++ module-6/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# client.py
+#
+# Simple client program to interface with my wacky module
+#
+# Noah S. Roberts
+# 03.10.2024
+# Assignment 9
+# for Module 6
+# Book Excercise ???
+# =============================================================================
+
