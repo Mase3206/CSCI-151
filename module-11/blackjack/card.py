39d7b7b065078e1bb805b96d25b82da63da253d5
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-04-22 19:42:54 +0000

    improve documentation, add repr

--- module-11/blackjack/card.py
+++ module-11/blackjack/card.py
@@ -44,6 +44,15 @@ strValues = [
 
 
 class Card:
+	"""
+	Card object.
+
+	Arguments
+	---------
+		suit (str): suit of the card. Must be plural; i.e. 'Hearts"
+		value (int): numerical representation of the card's value; i.e. 'Jack' = 11, 'Ace' = 1, or 'Three' = 3
+	"""
+
 	def __init__(self, suit: str, value: int):
 		if suit in suits:
 			self.suit = suit.title()
@@ -51,23 +60,49 @@ class Card:
 			raise ValueError(f'{suit} is not a valid suit name. Valid suits are {suits}.')
 		
 		self.set_value(value)
-
-
-	def _from_face(self, faceValue: str) -> int:
-		for i in range(len(strValues)):
-			if faceValue == strValues[i]:
-				return i + 1
 			
 	
 	def _from_numerical(self, value: int) -> str:
+		"""
+		Get str value from int value.
+
+		Arguments
+		---------
+			value (int): numerical representation of the card's value; i.e. 1, 3, or 11
+
+		Returns
+		-------
+			(str) string representation of the card's value; i.e. 'Ace', 'Three', or 'Jack'
+		"""
+
 		return strValues[value - 1]
 	
 	
-	def get_value(self) -> str:
+	def get_value(self) -> int:
+		"""
+		Return card's numerical value.
+
+		Returns
+		-------
+			(int) numerical representation of the card's value; i.e. 1, 3, or 11
+		"""
+
 		return self._value
 	
 
 	def set_value(self, value: int) -> None:
+		"""
+		Set both int and str values of card from given numerical representation of the card's value.
+
+		Arguments
+		---------
+			value (int): numerical representation of the card's value; i.e. 1, 3, or 11
+
+		Returns
+		-------
+			None
+		"""
+
 		if value in range(1, 14):
 			self._face = self._from_numerical(value)
 			self._value = value
@@ -75,13 +110,18 @@ class Card:
 			raise ValueError('Given value is not a valid card.')
 		
 	
+	# special methods
 	def __str__(self) -> str:
 		return f'{self._face} of {self.suit}'
+	
+	def __repr__(self) -> str:
+		return f'Card(suit={self.suit} _face={self._face} _value={self._value})'
 	
 
 def _tc():
 	a = Card('Spades', value=1)
 	print(a)
+	print(repr(a))
 
 if __name__ == '__main__':
 	_tc()
